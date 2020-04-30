92f6a00d361413e6cf3ba1f75d62c5c8fb5c7dfa
Cody Toney-Griffith <cody.toney@gmail.com>
2020-04-30 12:35:00 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+
+  attr_accessor :author, :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
+end
